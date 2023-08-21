dfc848eec1be575950c7edc003301295975d0882
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-08-21 15:08:06 +0100

    remove var

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,20 +1,20 @@
-var "dev_cidr" {
+variable "dev_cidr" {
     type = string 
     default = "10.0.0.0/16"
 }
-var "dev_subnet_one" {
+variable "dev_subnet_one" {
     type = string
     default = "10.10.0.0/24"
 }
-var "dev_subnet_two" {
+variable "dev_subnet_two" {
     type = string
     default = "10.10.0.128/24"
 }
-var "dev_natgw_one"  {
+variable "dev_natgw_one"  {
      type = string
     default = "10.10.0.0/24"
 }
-var "dev_env_name"   {
+variable "dev_env_name"   {
     type = string
     default = "Dev_Environment_VPC"
 }
--- test/variables.tf
+++ test/variables.tf
@@ -1,20 +1,20 @@
-var "test_cidr" {
+variable "test_cidr" {
     type = string 
     default = "10.11.0.0/16"
 }
-var "test_subnet_one" {
+variable "test_subnet_one" {
     type = string
     default = "10.11.0.0/24"
 }
-var "test_subnet_two" {
+variable "test_subnet_two" {
     type = string
     default = "10.11.0.128/24"
 }
-var "test_natgw_one"  {
+variable "test_natgw_one"  {
      type = string
     default = "10.11.0.0/24"
 }
-var "test_env_name"   {
+variable "test_env_name"   {
     type = string
     default = "Test_Environment_VPC"
 }
