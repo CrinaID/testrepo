78b90a59ecb10a7ba53c76002f823ecdc1fb577c
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-08-23 22:48:34 +0100

    remove vars

--- dev/main.tf
+++ dev/main.tf
@@ -2,8 +2,6 @@ module "vpcmodule"{
     source = "../modules/vpc"
 
     cidr_vpc = var.dev_cidr
-    private_subnets = var.private_subnets
-    public_subnets = var.public_subnets
    
     env_name = var.dev_env_name
 }
