d16b421c4f1f804504553270a84c393e9afa1414
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-08-24 11:15:52 +0100

    outputsdecl

--- dev/main.tf
+++ dev/main.tf
@@ -3,6 +3,9 @@ module "vpcmodule"{
 
     cidr_vpc = var.dev_cidr
     private_subnets = var.private_subnets
+    output "publicsubnets"{
+    value = var.public_subnets[0].id
+    } 
     public_subnets = var.public_subnets
     env_name = var.dev_env_name
 }
