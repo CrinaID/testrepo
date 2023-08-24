77dc2278fbd794500d78670ccd87c729f4c97328
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-08-24 15:43:07 +0100

    dev cirdr

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,6 +1,6 @@
 variable "dev_cidr" {
     type = string 
-    default = "10.0.0.0/16"
+    default = "10.10.0.0/16"
 }    
 variable "private_subnets" {
     default = ["10.10.3.0/24", "10.10.24.0/24"] 
