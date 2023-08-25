d1cc9a580d2c673df28c47e13e8972666802f6df
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-08-25 11:03:37 +0100

    dev test count

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -15,7 +15,9 @@ data "aws_availability_zones" "available" {}
 //condition ? true_val : false_val
 resource "aws_subnet" "public_subnets" {
   count = "${length(data.aws_availability_zones.available.names)}"
-  cidr_block = var.env_name == "DevEnv" ?  "10.10.${1+count.index}.0/24" : "10.11.${1+count.index}.0/24"
+  countDev = 1
+  countTest = 2
+  cidr_block = var.env_name == "DevEnv" ?  "10.10.${1+countDev.index}.0/24" : "10.11.${1+countTest.index}.0/24"
   availability_zone= "${data.aws_availability_zones.available.names[count.index]}"
   vpc_id   = aws_vpc.vpc_dev_test.id
   tags = {
@@ -26,7 +28,9 @@ resource "aws_subnet" "public_subnets" {
 resource "aws_subnet" "private_subnets" {
   
   count = "${length(data.aws_availability_zones.available.names)}"
-  cidr_block = var.env_name == "DevEnv" ?  "10.10.${count.index}.0/24" : "10.11.${1+count.index}.0/24"
+  countDev = 1
+  countTest = 2
+  cidr_block = var.env_name == "DevEnv" ?  "10.10.${1+countDev.index}.0/24" : "10.11.${1+countTest.index}.0/24"
   availability_zone= "${data.aws_availability_zones.available.names[count.index]}"
   vpc_id   = aws_vpc.vpc_dev_test.id
   tags = {
