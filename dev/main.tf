a23e51cf79683d5bb0cc5db0e821af49147d4dc6
CrinaID <117096090+CrinaID@users.noreply.github.com>
2023-08-25 17:36:16 +0100

    add eke

--- dev/main.tf
+++ dev/main.tf
@@ -6,4 +6,32 @@ module "vpcmodule"{
     public_subnets = var.public_subnets
     env_name = var.dev_env_name
     project_code = var.project_code
+
+    cluster_name    = var.cluster_name
+    cluster_version = "1.24"
+    subnet_ids      = var.private_subnets
+    cluster_endpoint_public_access  = true
+
+    cluster_addons = {
+      coredns = {
+        most_recent = true
+      }
+      kube-proxy = {
+        most_recent = true
+      }
+      vpc-cni = {
+        most_recent = true
+      }
+    }
+    vpc_id = module.vpc_dev_test.vpc_id
+
+    eks_managed_node_groups = {
+        first = {
+        desired_capacity = 1
+        max_capacity     = 3
+        min_capacity     = 1
+
+        instance_type = "t2.micro"
+        }
+    }
 }
