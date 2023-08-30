8743a556a829d2e140fb2805fd805d9760e5b7ed
CrinaID <117096090+CrinaID@users.noreply.github.com>
2023-08-30 12:41:35 +0100

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -74,7 +74,8 @@ resource "aws_eip" "Nat-Gateway-EIP" {
 
 resource "aws_nat_gateway" "nat_gateway_one" {
   depends_on = [
-    aws_eip.Nat-Gateway-EIP
+    aws_eip.Nat-Gateway-EIP,
+    aws_internet_gateway.internet_gateway
   ]
  
   # Allocating the Elastic IP to the NAT Gateway!
@@ -88,3 +89,89 @@ resource "aws_nat_gateway" "nat_gateway_one" {
   }
 }
 
+//IAM role for EKS - used to make API calls to AWS services
+//i.e. to create managed node pools
+
+
+resource "aws_iam_role" "eks-cluster" {
+  name = "eks-cluster-${var.cluster_name}"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+// Attach AmazonEKSClusterPolicy
+
+resource "aws_iam_role_policy_attachment" "amazon-eks-cluster-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+
+//EKS cluster
+
+resource "aws_eks_cluster" "cluster" {
+  name     = var.cluster_name
+  version  = var.cluster_version
+  role_arn = aws_iam_role.eks-cluster.arn
+
+  vpc_config {
+
+    endpoint_private_access = false
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+
+    subnet_ids = [aws_subnet.private_subnets[*].id]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.amazon-eks-cluster-policy]
+}
+
+
+resource "aws_iam_role" "eks-fargate-profile" {
+  name = "eks-fargate-profile"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks-fargate-profile" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.eks-fargate-profile.name
+}
+
+resource "aws_eks_fargate_profile" "kube-system" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "kube-system"
+  pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
+
+  # These subnets must have the following resource tag: 
+  # kubernetes.io/cluster/<CLUSTER_NAME>.
+  subnet_ids = [
+    aws_subnet.private_subnets[*].id
+  ]
+
+  selector {
+    namespace = "kube-system"
+  }
+}
