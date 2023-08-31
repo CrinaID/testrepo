a138d388e375e73f3d6ffef62302403fe54cf022
CrinaID <117096090+CrinaID@users.noreply.github.com>
2023-08-31 15:02:05 +0100

    set count

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -134,13 +134,13 @@ resource "aws_eks_cluster" "cluster" {
   name     = var.cluster_name
   version  = var.cluster_version
   role_arn = aws_iam_role.eks-cluster.arn
-
+  count = "${length(var.private_subnets)}"
   vpc_config {
 
     endpoint_private_access = false
     endpoint_public_access  = true
     public_access_cidrs     = ["0.0.0.0/0"]
-    count = "${length(var.private_subnets)}"
+    
     subnet_ids = [
       aws_subnet.private_subnets[count.index].id
     ]
