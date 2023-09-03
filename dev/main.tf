821275545dfa944d57868ea03f73387124c8e72a
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-03 16:43:23 +0100

    subnet id rm

--- dev/main.tf
+++ dev/main.tf
@@ -8,6 +8,6 @@ module "vpcmodule"{
     project_code = var.project_code
     cluster_name = var.cluster_name
     cluster_version = var.cluster_version
-    public_subnet_ids = output.public_subnet_ids.value
-    private_subnet_ids = output.private_subnet_ids.value
+    //public_subnet_ids = output.public_subnet_ids.value
+    //private_subnet_ids = output.private_subnet_ids.value
 }
