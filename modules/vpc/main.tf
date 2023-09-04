3fe7f310975d5371ca2904447dd5b247576ffc02
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-04 18:53:41 +0100

    add tags

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -30,7 +30,9 @@ resource "aws_subnet" "public_subnets" {
   availability_zone= "${data.aws_availability_zones.available.names[count.index]}"
   vpc_id   = aws_vpc.vpc_dm_eks.id
   tags = {
+    
     Name = "${var.project_code}-${var.env_name}-publicsub-${1+count.index}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 }
 
@@ -42,6 +44,7 @@ resource "aws_subnet" "private_subnets" {
   vpc_id   = aws_vpc.vpc_dm_eks.id
   tags = {
     Name = "${var.project_code}-${var.env_name}-privatesub-${1+count.index}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 }
 
@@ -229,3 +232,20 @@ EOH
 }
 
 */
+
+resource "aws_eks_fargate_profile" "staging" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "staging"
+  pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
+
+  # These subnets must have the following resource tag: 
+  # kubernetes.io/cluster/<CLUSTER_NAME>.
+  subnet_ids = [
+    aws_subnet.private-us-east-1a.id,
+    aws_subnet.private-us-east-1b.id
+  ]
+
+  selector {
+    namespace = "staging"
+  }
+}
