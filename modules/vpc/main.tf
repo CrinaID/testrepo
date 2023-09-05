aa64be57c90b013df055589d9ae307bb34124526
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-05 12:59:46 +0100

    helm

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -291,13 +291,14 @@ output "aws_load_balancer_controller_role_arn" {
 
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.cluster.endpoint
+    config_path = "~/.kube/config"
+    /*host                   = aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
       command     = "aws"
-    }
+    }*/
   }
 }
 /*
