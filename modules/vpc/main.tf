e5e41c7a15f3f0317736b1bac6da85e0911c5dc5
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-05 13:10:50 +0100

    cluster endpoint

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -292,7 +292,7 @@ output "aws_load_balancer_controller_role_arn" {
 provider "helm" {
   kubernetes {
     //config_path = "~/.kube/config"
-    host                   = aws_eks_cluster.cluster.endpoint
+    host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
