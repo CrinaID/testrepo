ab5d5bde3c647e496855db984ecd245046a1731d
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-06 14:37:04 +0100

    cluster auth

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -296,16 +296,21 @@ resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" 
 output "aws_load_balancer_controller_role_arn" {
   value = aws_iam_role.aws_load_balancer_controller.arn
 }
-
+data "aws_eks_cluster_auth" "cluster-auth" {
+  depends_on = [aws_eks_cluster.cluster]
+  name       = aws_eks_cluster.cluster.name
+}
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
-    exec {
+    /*exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
       command     = "aws"
-    }
+    }*/
+    token                  = data.aws_eks_cluster_auth.cluster-auth.token
+    load_config_file       = false
   }
 }
 /*
