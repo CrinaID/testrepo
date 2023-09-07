5994696438c48d43935927096d243bc0883a59f2
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-07 11:59:45 +0100

    subnet ids

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -312,7 +312,11 @@ resource "aws_eks_fargate_profile" "main" {
   cluster_name           = aws_eks_cluster.main.name
   fargate_profile_name   = "fp-default"
   pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
-  subnet_ids             = var.private_subnets.*.id
+      subnet_ids = [
+        aws_subnet.private_subnets[0].id,
+        aws_subnet.private_subnets[1].id
+
+    ]
 
   selector {
     namespace = "default"
