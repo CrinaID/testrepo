99f7cc1a7103fbeef5637dc400c22d0dc463e56b
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-07 12:22:39 +0100

    old code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,5 +1,7 @@
+Main.tf
+
 provider "aws" {
-  region = var.region
+  region = "eu-north-1"
 }
 
 terraform {
@@ -34,7 +36,6 @@ resource "aws_subnet" "public_subnets" {
     Name = "${var.project_code}-${var.env_name}-publicsub-${1+count.index}"
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
     "kubernetes.io/role/internal-elb"           = "1"
-    "kubernetes.io/role/elb"= "1"
   }
 }
 
@@ -48,7 +49,6 @@ resource "aws_subnet" "private_subnets" {
     Name = "${var.project_code}-${var.env_name}-privatesub-${1+count.index}"
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
     "kubernetes.io/role/internal-elb"           = "1"
-    "kubernetes.io/role/elb"= "1"
   }
 }
 
@@ -106,14 +106,62 @@ resource "aws_nat_gateway" "nat_gateway_one" {
   }
 }
 
-resource "aws_eks_cluster" "main" {
+
+
+//IAM role for EKS - used to make API calls to AWS services
+//i.e. to create managed node pools
+
+
+resource "aws_iam_role" "eks-cluster" {
+  name = "eks-cluster-${var.cluster_name}"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+// Attach AmazonEKSClusterPolicy
+
+resource "aws_iam_role_policy_attachment" "amazon-eks-cluster-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+
+//EKS cluster
+
+resource "aws_eks_cluster" "cluster" {
   name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = var.cluster_version
+  role_arn = aws_iam_role.eks-cluster.arn
+  //count = "${length(aws_subnet.private_subnets)}"
 
-  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  /*enable_cluster_autoscaler = true
+  enable_aws_load_balancer_controller   = true
+  enable_metrics_server = true
 
+   metrics_server_helm_config = {
+    values = [templatefile("${path.module}/k8s/metrics-server-values.yaml", {
+      operating_system = "linux"
+    })]
+  }*/
   vpc_config {
-      subnet_ids = [
+
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+    //need to improve this code and not use 0 and 1 
+    subnet_ids = [
         aws_subnet.private_subnets[0].id,
         aws_subnet.public_subnets[0].id,
         aws_subnet.private_subnets[1].id,
@@ -121,235 +169,224 @@ resource "aws_eks_cluster" "main" {
     ]
   }
 
-  timeouts {
-    delete = "30m"
-  }
-
-  depends_on = [
-    aws_cloudwatch_log_group.eks_cluster,
-    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.AmazonEKSServicePolicy
-  ]
-}
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.10"
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = aws_eks_cluster.main.id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.main.id
-}
-
-resource "aws_iam_policy" "AmazonEKSClusterCloudWatchMetricsPolicy" {
-  name   = "AmazonEKSClusterCloudWatchMetricsPolicy"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "cloudwatch:PutMetricData"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        }
-    ]
-}
-EOF
+  depends_on = [aws_iam_role_policy_attachment.amazon-eks-cluster-policy]
 }
 
 
-resource "aws_iam_role" "eks_cluster_role" {
-  name                  = "${var.project_code}-eks-${var.env_name}-cluster-role"
-  force_detach_policies = true
+resource "aws_iam_role" "eks-fargate-profile" {
+  name = "eks-fargate-profile"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": [
-          "eks.amazonaws.com",
-          "eks-fargate-pods.amazonaws.com"
-          ]
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSCloudWatchMetricsPolicy" {
-  policy_arn = aws_iam_policy.AmazonEKSClusterCloudWatchMetricsPolicy.arn
-  role       = aws_iam_role.eks_cluster_role.name
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
 }
 
 
-resource "aws_cloudwatch_log_group" "eks_cluster" {
-  name              = "/aws/eks/${var.cluster_name}/cluster"
-  retention_in_days = 30
-
-  tags = {
-    Name        = "${var.env_name}-eks-cloudwatch-log-group"
-    Environment = var.env_name
-  }
-}
-
-resource "aws_eks_node_group" "main" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "kube-system"
-  node_role_arn   = aws_iam_role.eks_node_group_role.arn
-  subnet_ids = [
-      aws_subnet.private_subnets[0].id,
-      aws_subnet.private_subnets[1].id
-    ]
-  scaling_config {
-    desired_size = 4
-    max_size     = 4
-    min_size     = 4
-  }
-
-  instance_types  = ["t2.micro"]
-
-  tags = {
-    Name        = "dm-eks-${var.env_name}-node-group"
-
-  }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-  ]
-}
-
-resource "aws_iam_role" "eks_node_group_role" {
-  name                  = "dm-eks-node-group-role"
-  force_detach_policies = true
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": [
-          "ec2.amazonaws.com"
-          ]
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_group_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node_group_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_node_group_role.name
-}
-
-data "template_file" "kubeconfig" {
-  template = file("${path.module}/templates/kubeconfig.tpl")
-
-  vars = {
-    kubeconfig_name           = "eks_${aws_eks_cluster.main.name}"
-    clustername               = aws_eks_cluster.main.name
-    endpoint                  = data.aws_eks_cluster.cluster.endpoint
-    cluster_auth_base64       = data.aws_eks_cluster.cluster.certificate_authority[0].data
-  }
-}
-
-resource "local_file" "kubeconfig" {
-  content  = data.template_file.kubeconfig.rendered
-  filename = pathexpand("~/.kube/config")
-}
-
-resource "aws_iam_openid_connect_provider" "main" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.external.thumbprint.result.thumbprint]
-  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
-}
-data "external" "thumbprint" {
-  program =    ["${path.module}/oidc_thumbprint.sh", var.region]
-  depends_on = [aws_eks_cluster.main]
-}
-
-resource "aws_eks_fargate_profile" "main" {
-  cluster_name           = aws_eks_cluster.main.name
-  fargate_profile_name   = "fp-default"
-  pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
-      subnet_ids = [
-        aws_subnet.private_subnets[0].id,
-        aws_subnet.private_subnets[1].id
-
-    ]
-
-  selector {
-    namespace = "default"
-  }
-
-  selector {
-    namespace = "2048-game"
-  }
-}
-resource "aws_iam_role_policy_attachment" "AmazonEKSFargatePodExecutionRolePolicy" {
+resource "aws_iam_role_policy_attachment" "eks-fargate-profile" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
-  role       = aws_iam_role.fargate_pod_execution_role.name
+  role       = aws_iam_role.eks-fargate-profile.name
 }
 
-resource "aws_iam_role" "fargate_pod_execution_role" {
-  name                  = "dm-eks-fargate-pod-execution-role"
-  force_detach_policies = true
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": [
-          "eks.amazonaws.com",
-          "eks-fargate-pods.amazonaws.com"
-          ]
-      },
-      "Action": "sts:AssumeRole"
-    }
+resource "aws_eks_fargate_profile" "kube-system" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "kube-system"
+  pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
+
+  //count = "${length(aws_subnet.private_subnets)}"
+  subnet_ids = [
+    aws_subnet.private_subnets[0].id,
+    aws_subnet.private_subnets[1].id
   ]
+
+  selector {
+    namespace = "kube-system"
+  }
 }
-POLICY
+
+resource "aws_eks_fargate_profile" "staging" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "staging"
+  pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
+
+  # These subnets must have the following resource tag: 
+  # kubernetes.io/cluster/<CLUSTER_NAME>.
+  subnet_ids = [
+    aws_subnet.private_subnets[0].id,
+    aws_subnet.private_subnets[1].id
+  ]
+
+  selector {
+    namespace = "staging"
+  }
 }
+//remove ec2 annotation from CoreDNS deployment
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.cluster.id
+}
+
+resource "null_resource" "k8s_patcher" {
+  depends_on = [aws_eks_fargate_profile.kube-system]
+
+  triggers = {
+    endpoint = aws_eks_cluster.cluster.endpoint
+    ca_crt   = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    token    = data.aws_eks_cluster_auth.eks.token
+  }
+
+  provisioner "local-exec" {
+    command = <<EOH
+cat >/tmp/ca.crt <<EOF
+${base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)}
+EOF
+kubectl \
+  --server="${aws_eks_cluster.cluster.endpoint}" \
+  --certificate_authority=/tmp/ca.crt \
+  --token="${data.aws_eks_cluster_auth.eks.token}" \
+  patch deployment coredns \
+  -n kube-system --type json \
+  -p='[{"op": "remove", "path": "/spec/template/metadata/annotations/eks.amazonaws.com~1compute-type"}]'
+EOH
+  }
+
+  lifecycle {
+    ignore_changes = [triggers]
+  }
+}
+/*
+data "aws_eks_cluster_auth" "cluster-auth" {
+  depends_on = [aws_eks_cluster.cluster]
+  name       = aws_eks_cluster.cluster.name
+}
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    /*exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
+      command     = "aws"
+    }
+    token                  = data.aws_eks_cluster_auth.cluster-auth.token
+    
+  }
+}
+/*
+resource "helm_release" "metrics-server" {
+    name = "metrics-server"
+
+    repository       = "https://charts.bitnami.com/bitnami"
+    chart            = "metrics-server"
+    namespace        = "metrics-server"
+    //version          = "5.11.1"
+    create_namespace = true
+
+    set {
+        name  = "apiService.create"
+        value = "true"
+    }
+
+  depends_on = [aws_eks_fargate_profile.kube-system]
+}
+
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+
+data "aws_iam_policy_document" "aws_load_balancer_controller_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  assume_role_policy = data.aws_iam_policy_document.aws_load_balancer_controller_assume_role_policy.json
+  name               = "aws-load-balancer-controller"
+}
+
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  policy = file("${path.module}/LBControllerTF.json")
+  name   = "LBControllerTF"
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+}
+
+
+resource "helm_release" "aws-load-balancer-controller" {
+  name = "aws-load-balancer-controller"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version = "1.3.3"
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.cluster.id
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v2.4.2"
+  }
+
+  set {
+    name  = "replicaCount"
+    value = 1
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.aws_load_balancer_controller.arn
+  }
+
+  # EKS Fargate specific
+  set {
+    name  = "region"
+    value = "eu-north-1"
+  }
+
+  set {
+    name  = "vpcId"
+    value = aws_vpc.vpc_dm_eks.id
+  }
+
+  depends_on = [aws_eks_fargate_profile.kube-system]
+}
+*/
