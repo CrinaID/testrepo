f53fcba07d3726961f2ad490623493025235ab58
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-08 14:33:57 +0100

    gateway id

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -135,7 +135,7 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_internet_gateway.internet_gateway.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
   tags = {
