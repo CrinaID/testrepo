da4811ccbf5ed7b6d9fb882f129867b2b1f9dec9
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-11 11:04:38 +0100

    backend

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,15 +1,59 @@
-
 provider "aws" {
-  region = "eu-north-1"
+  region = var.region
 }
 
-terraform {
-  backend "s3" {
-    bucket = "dm-gen-configuration"
-    key    = "/"
-    region = "eu-west-1"
+resource "aws_kms_key" "state_backend_bucket_kms_key" {
+  description             = "Encrypt the state bucket objects"
+  deletion_window_in_days = 10
+}
+resource "aws_s3_bucket" "state_backend_bucket" {
+  bucket = "dm-${var.env_name}-configuration"
+  lifecycle {
+    prevent_destroy = true
   }
 }
+resource "aws_s3_bucket_versioning" "state_backend_bucket_versioning" {
+  bucket  = aws_s3_bucket.state_backend_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_backend_bucket_encryption" {
+  bucket = aws_s3_bucket.state_backend_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.state_backend_bucket_kms_key.arn
+      sse_algorithm = "aws:kms"
+    }
+    bucket_key_enabled = true
+  }
+}
+
+# block S3 bucket public access per each Env's bucket
+resource "aws_s3_bucket_public_access_block" "state_backend_bucket_acl" {
+  bucket = aws_s3_bucket.state_backend_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# create DynamoDB table per each Env
+resource "aws_dynamodb_table" "state_dynamo_table" {
+  name = "tf-state-lock-${var.env_name}"
+
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
 
 resource "aws_vpc" "vpc_dm_eks" {
   cidr_block = var.cidr_vpc
