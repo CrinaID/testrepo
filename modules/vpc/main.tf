21472dcaa04c5548802837d3361028c460e7e41d
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-13 11:44:14 +0100

    replace efs code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -467,61 +467,30 @@ resource "helm_release" "aws-load-balancer-controller" {
   depends_on = [aws_eks_fargate_profile.kube-system]
 }
 
-# Creating Amazon EFS File system
-resource "aws_efs_file_system" "myfilesystem" {
-# Creating the AWS EFS lifecycle policy
-# Amazon EFS supports two lifecycle policies. Transition into IA and Transition out of IA
-# Transition into IA transition files into the file systems's Infrequent Access storage class
-# Transition files out of IA storage
-  lifecycle_policy {
-    transition_to_ia = "AFTER_30_DAYS"
-  }
-# Tagging the EFS File system with its value as Myfilesystem
+resource "aws_efs_file_system" "eks" {
+  creation_token = "eks"
+
+  performance_mode = "generalPurpose"
+  throughput_mode  = "bursting"
+  encrypted        = true
+
+  # lifecycle_policy {
+  #   transition_to_ia = "AFTER_30_DAYS"
+  # }
+
   tags = {
-    Name = "${var.project_code}-efs-${var.env_name}"
+    Name = "dm-eks-filesystem"
   }
 }
 
-# Creating the EFS access point for AWS EFS File system
-resource "aws_efs_access_point" "test" {
-  file_system_id = aws_efs_file_system.myfilesystem.id
+resource "aws_efs_mount_target" "zone-a" {
+  file_system_id  = aws_efs_file_system.eks.id
+  subnet_id       = aws_subnet.private_subnets[0].id
+  security_groups = [aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id]
 }
-# Creating the AWS EFS System policy to transition files into and out of the file system.
-resource "aws_efs_file_system_policy" "policy" {
-  file_system_id = aws_efs_file_system.myfilesystem.id
-# The EFS System Policy allows clients to mount, read and perform 
-# write operations on File system 
-# The communication of client and EFS is set using aws:secureTransport Option
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Id": "Policy01",
-    "Statement": [
-        {
-            "Sid": "Statement",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "*"
-            },
-            "Resource": "${aws_efs_file_system.myfilesystem.arn}",
-            "Action": [
-                "elasticfilesystem:ClientMount",
-                "elasticfilesystem:ClientRootAccess",
-                "elasticfilesystem:ClientWrite"
-            ],
-            "Condition": {
-                "Bool": {
-                    "aws:SecureTransport": "false"
-                }
-            }
-        }
-    ]
-}
-POLICY
-}
-# Creating the AWS EFS Mount point in a specified Subnet 
-# AWS EFS Mount point uses File system ID to launch.
-resource "aws_efs_mount_target" "alpha" {
-  file_system_id = aws_efs_file_system.myfilesystem.id
-  subnet_id      = aws_subnet.private_subnets[0].id
+
+resource "aws_efs_mount_target" "zone-b" {
+  file_system_id  = aws_efs_file_system.eks.id
+  subnet_id       = aws_subnet.private_subnets[1].id
+  security_groups = [aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id]
 }
