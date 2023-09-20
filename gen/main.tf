6f337dd258ddb22c0c117822c53a4ba5239b034d
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-20 15:35:24 +0100

    ddb module main

--- gen/main.tf
+++ gen/main.tf
@@ -37,19 +37,6 @@ resource "aws_s3_bucket_public_access_block" "state_backend_bucket_acl" {
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "state_dynamo_table" {
-  name = "tf-state-lock-${var.env_name}"
-
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
-
 // DEVOPS ROLE
 
 # Create an IAM policy
