db5ae2d025faf2a3292c1133410f0562e55a604e
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-27 17:26:28 +0100

    helm provider this

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -141,18 +141,26 @@ EOH
     ignore_changes = [triggers]
   }
 }
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
 
 
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.this.certificate_authority[0].data)
-    exec {
+    /*exec {
       api_version = "client.authentication.k8s.io/v1beta1"
 
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
       command     = "aws"
-    }   
+    } */
+    token                  = data.aws_eks_cluster_auth.this.token  
   }
 }
 /*
