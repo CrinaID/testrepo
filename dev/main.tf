01b32d8177df87150e9d2bad2c376fe33dc35377
CrinaID <crina-ioana.dascal@tpximpact.com>
2023-09-28 14:43:39 +0100

    lb dev

--- dev/main.tf
+++ dev/main.tf
@@ -32,7 +32,7 @@ module "eks_cluster"{
 
 }
 module "load_balancer" {
-    source = "../modules/load-balancer"
+    source = "../modules/load-balancer-dev"
     vpc_id = module.vpcmodule.vpc.id
     eks_cluster = module.eks_cluster.eks_cluster
     env_name = var.dev_env_name
